307c7e63c145d83901835c8bc824b8cfa1d70193
Ronald Stephens <rstephens@bluemorpho.cc>
2019-10-12 16:54:12 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,7 +1,11 @@
 class AddGradeAndBirhtdateToStudents < ActiveRecord::Migration
 
-def
-
+  def change
+    add_colunm :students do |c|
+      c.integer :grade
+      c.string :birthdate
+    end
+  end  
 
 
 end
